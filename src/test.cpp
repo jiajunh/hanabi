f53c7737b06e79f46224660c5e434c24c37b0601
Jiajun Hong <jiajunh@Jiajuns-MacBook-Pro.local>
2022-11-07 21:14:45 +0000

    hanabi

---
+++ src/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <SysConfig.h>
+// #include <GL/glew.h>
+#include <glfw/glfw3.h>
+
+
+int main() {
+    std::cout<<"woow"<<std::endl;
+    std::cout<<HANABI_VERSION_MAJOR<<", "<<HANABI_VERSION_MINOR<<std::endl;
+    return 0;
+}
